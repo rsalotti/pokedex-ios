22cf94afa114feb4d271e6b44d69cf5db246448d
Ricardo Santos <40443486+rsalotti@users.noreply.github.com>
2024-08-13 12:10:38 -0300

    Initial Commit

---
+++ Pokedex Watch App/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex Watch App
+//
+//  Created by Ricardo Santos on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
